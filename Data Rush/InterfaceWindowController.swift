48d4dc25054d312955d78605bea8c2cc7684293a
Runkai Zhang <runkaizh@numericdesign.org>
2018-05-30 15:06:48 -0400

    Added Interface Window Controller

---
+++ Data Rush/InterfaceWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  InterfaceWindowController.swift
+//  Data Rush
+//
+//  Created by Runkai Zhang on 5/30/18.
+//  Copyright © 2018 Runkai Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class InterfaceWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
