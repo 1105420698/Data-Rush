4f390a3536c801e51ba48985ca2c9579639e4edf
Runkai Zhang <runkaizh@numericdesign.org>
2018-05-30 14:14:09 -0400

    Added a new View Controller

---
+++ Data Rush/InterfaceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InterfaceViewController.swift
+//  Data Rush
+//
+//  Created by Runkai Zhang on 5/30/18.
+//  Copyright © 2018 Runkai Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class InterfaceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
