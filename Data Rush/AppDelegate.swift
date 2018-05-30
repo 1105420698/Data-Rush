091ba08f95509bae78e48f3c38ec3015f8f56499
Runkai Zhang <runkaizh@numericdesign.org>
2018-05-30 14:03:50 -0400

    Initial Commit

---
+++ Data Rush/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Data Rush
+//
+//  Created by Runkai Zhang on 5/30/18.
+//  Copyright © 2018 Runkai Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Data Rush/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Data Rush
+//
+//  Created by Runkai Zhang on 5/30/18.
+//  Copyright © 2018 Runkai Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
